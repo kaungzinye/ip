07611e438ed072abe3668fcd0178ffb4556fe3fc
Kaung Z Y Naung <kaungzinye@Kaungs-Laptop.local>
2025-01-23 17:04:07 +0800

    Finished level 0

---
+++ src/main/java/GojoSatoru.java
@@ -0,0 +1,11 @@
+public class GojoSatoru {
+    public static void main(String[] args) {
+        String text = "____________________________________________________________\n" +
+            " Hello! I'm Gojo Satoru \n" +
+            " What can I do for you?\n" +
+            "____________________________________________________________\n" +
+            " Bye. Hope to see you again soon!\n" +
+            "____________________________________________________________\n";
+        System.out.println(text);
+    }
+}
