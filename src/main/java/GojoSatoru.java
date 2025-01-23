04610d2281ddb119691093a890bfeb832132bcac
Kaung Z Y Naung <kaungzinye@Kaungs-Laptop.local>
2025-01-23 17:29:17 +0800

    Please enter the commit message for your changes. Lines starting

--- src/main/java/GojoSatoru.java
+++ src/main/java/GojoSatoru.java
@@ -1,11 +1,24 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class GojoSatoru {
     public static void main(String[] args) {
-        String text = "____________________________________________________________\n" +
-            " Hello! I'm Gojo Satoru \n" +
-            " What can I do for you?\n" +
-            "____________________________________________________________\n" +
-            " Bye. Hope to see you again soon!\n" +
-            "____________________________________________________________\n";
-        System.out.println(text);
+        String introText = "   ____________________________________________________________\n" +
+            "   Hello! I'm Gojo Satoru\n" +
+            "   Am I the strongest chatbot because I'm Gojo Satoru\n" +
+            "   or am I the Gojo Statoru because I am the weakest chatbot?\n" +
+            "   What can I do for you?\n" + "   ____________________________________________________________\n";
+        System.out.println(introText);
+        Scanner userScanner = new Scanner(System.in);
+        String userInput = userScanner.nextLine();
+        while (!Objects.equals(userInput, "bye")){
+            System.out.println("   ____________________________________________________________\n   " +
+                userInput + "\n"
+            + "   ____________________________________________________________\n");
+            userInput = userScanner.nextLine();
+        }
+        System.out.println( "   ____________________________________________________________\n  " +
+            " You're weak... the next time I see you, I'd win.\n"
+            + "   ____________________________________________________________\n");
     }
 }
